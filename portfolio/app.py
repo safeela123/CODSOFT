0960a4e5183fe30fc19da6fc4a3c5a7f67c8497d
safeela123 <safeelansri42484@gmail.com>
2026-03-11 21:25:31 +0530

    Fixed portfolio folder

---
+++ portfolio/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template,request,redirect,flash,url_for,send_from_directory
+from flask_mail import Mail,Message
+import os
+app=Flask(__name__)
+app.secret_key=''
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']='safeelansrin42484@gmail.com'
+app.config['MAIL_PASSWORD']='..........'
+mail=Mail(app)
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DOWNLOAD_FOLDER = os.path.join(BASE_DIR, 'downloads')
+
+@app.route('/')
+def index():
+    
+    return render_template('main.html')
+
+@app.route('/#skills')
+def skills():
+    
+    return render_template('main.html')
+
+@app.route('/send_message',methods=['POST'])
+def send_message():
+    name=request.form.get('name')
+    email=request.form.get('email')
+    message_text=request.form.get('message')
+    
+    # compose email
+    msg=Message(
+        subject=f"new message from {name}",
+        sender='safeelansrin42484@gmail.com',
+        recipients=[app.config['MAIL_USERNAME']], # RECIEVE THE MESSAGE
+        body=f"From:{name}<{email}>\n\n{message_text}"
+    )
+    try:
+        mail.send(msg)
+        flash('Message sent successfully!','success')
+    except Exception as e:
+        print(f"Failed to send Email:{e}")
+        flash('Error while sending message.','error')
+    return redirect('/#contactme')
+
+@app.route('/#project')
+def project():
+    return render_template('main.html')
+
+@app.route('/#contactme')
+def contact():
+    return render_template('main.html')
+
+DOWNLOAD_FOLDER = os.path.join(app.root_path, 'downloads')
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(DOWNLOAD_FOLDER,filename, as_attachment=True)
+
+if __name__=='__main__':
+    app.run(debug=True)
